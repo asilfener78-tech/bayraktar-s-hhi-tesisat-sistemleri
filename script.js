dcf082e97aa4354f85a738f025cd062270820483
asilfener78-tech <asilfener78@gmail.com>
2026-07-08 22:28:54 +0300

    Bayraktar Sıhhi Tesisatlar Sistemi
    Services ID update

--- script.js
+++ script.js
@@ -286,11 +286,11 @@ form.addEventListener("submit", function (e) {
 
 
   emailjs.init({
-    publicKey: "rBL0dJQRPynTjxJ4E",
+    publicKey: "2QdVZXj9UMxPGZeYN",
   });
 
   emailjs
-    .sendForm("service_jy22k7q", "template_7hqg3rc", form)
+    .sendForm("service_krrjnrs", "template_7d9zge7", form)
 
     .then(function () {
       alert("Mesaj gönderildi.");
